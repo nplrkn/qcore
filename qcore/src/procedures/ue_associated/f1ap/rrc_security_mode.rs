c251b72c634e4b9d5c1cbc8703b208938b3372db
Nic Larkin <nplrkn@gmail.com>
2025-06-07 09:22:01 +0200

    Detect security mode failure

--- qcore/src/procedures/ue_associated/f1ap/rrc_security_mode.rs
+++ qcore/src/procedures/ue_associated/f1ap/rrc_security_mode.rs
@@ -1,5 +1,6 @@
 use super::prelude::*;
 use f1ap::SrbId;
+use rrc::{C1_6, UlDcchMessage, UlDcchMessageType};
 
 define_ue_procedure!(RrcSecurityModeProcedure);
 
@@ -8,9 +9,16 @@ impl<'a, A: HandlerApi> RrcSecurityModeProcedure<'a, A> {
         self.configure_rrc_security(kgnb);
         let r = crate::rrc::build::security_mode_command(1);
         self.log_message("<< RrcSecurityModeCommand");
-        let _rrc_security_mode_complete = self.rrc_request(SrbId(1), &r).await;
-        self.log_message(">> RRcSecurityModeComplete");
-        Ok(self.0)
+        let response = self.rrc_request(SrbId(1), &r).await?;
+        match *response {
+            UlDcchMessage {
+                message: UlDcchMessageType::C1(C1_6::SecurityModeComplete(_)),
+            } => {
+                self.log_message(">> RrcSecurityModeComplete");
+                Ok(self.0)
+            }
+            m => bail!("Expected Rrc SecurityModeComplete, received {:?}", m),
+        }
     }
 
     fn configure_rrc_security(&mut self, kgnb: &[u8; 32]) {
