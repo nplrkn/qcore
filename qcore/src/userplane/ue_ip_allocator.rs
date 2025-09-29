acc51efff78a9a44068c0d9a9286fc66c3f6c5cf
Nic Larkin <nplrkn@gmail.com>
2025-09-29 11:23:50 +0200

    Send DCHP request + receive ack

--- qcore/src/userplane/ue_ip_allocator.rs
+++ qcore/src/userplane/ue_ip_allocator.rs
@@ -1,10 +1,15 @@
 use crate::{data::UeIpAllocationConfig, userplane::netlink_route::NetlinkRouteProgrammer};
-use anyhow::{Result, anyhow, bail, ensure};
+use anyhow::{Result, anyhow, bail};
+use async_std::{
+    channel::{Receiver, Sender},
+    sync::Mutex,
+};
 use dhcproto::{
     Decodable, Decoder, Encodable, Encoder,
     v4::{self, Message, MessageType},
 };
-use slog::{Logger, warn};
+use slog::{Logger, debug, warn};
+use smol::net::UdpSocket;
 use std::{
     collections::HashMap,
     net::{Ipv4Addr, SocketAddr},
@@ -62,14 +67,7 @@ impl UeIpAllocator {
                 ue_addr_octets[3] = idx;
                 Ipv4Addr::from(ue_addr_octets)
             }
-            UeIpAllocationMode::Dhcp(dhcp_client) => {
-                // Obtain a new address and store the lease.
-                let lease = DhcpLease::new(&dhcp_client).await?;
-
-                // TODO: store the lease handle in a map
-
-                lease.ip
-            }
+            UeIpAllocationMode::Dhcp(dhcp_client) => dhcp_client.obtain_lease(logger).await?,
         };
 
         // Program a host route for it (which enables Linux proxy ARP + UE packet reception by ebpf).
@@ -80,13 +78,15 @@ impl UeIpAllocator {
         Ok(addr)
     }
 
-    // TODO: we shouldn't hold up the release process until we get a response from the server.
-    // It should be a separate task. See comment on drop() below.
     pub async fn release(&self, addr: Ipv4Addr, logger: &Logger) {
-        // TODO: look up the lease cancel handle in a map + cancel it
-        // if let Some(lease) = addr.handle {
-        //     lease.cancel().await;
-        // }
+        match &self.mode {
+            UeIpAllocationMode::RoutedUeSubnet(_ue_subnet) => {}
+            UeIpAllocationMode::Dhcp(dhcp_client) => {
+                if let Err(e) = dhcp_client.cancel_lease(&addr).await {
+                    warn!(logger, "DHCP cancel lease failed - {e}")
+                }
+            }
+        };
 
         self.netlink_route_programmer
             .delete_host_route(addr, logger)
@@ -94,19 +94,19 @@ impl UeIpAllocator {
     }
 }
 
-use async_std::{channel::Sender, sync::Mutex};
-use smol::net::UdpSocket;
-
 // The DHCP client owns a UDP socket listening on port 68.
 // It sends requests over its socket and routes responses back to the requester.
 // It can cope with multiple parallel transactions from different clients.
+#[derive(Clone)]
 struct DhcpClient {
     socket: UdpSocket,
     local_mac: [u8; 6],
-
-    // This is keyed on the DHCP XID
-    pending_requests: Arc<Mutex<HashMap<u32, Sender<Message>>>>,
+    pending_requests: Arc<Mutex<HashMap<Xid, Sender<Message>>>>,
+    leases: Arc<Mutex<HashMap<Ipv4Addr, Sender<()>>>>,
 }
+type Xid = u32;
+
+const DHCP_RESPONSE_TIMEOUT_MS: u64 = 1000;
 
 impl DhcpClient {
     async fn new(local_mac: [u8; 6], logger: &Logger) -> Result<Self> {
@@ -125,21 +125,74 @@ impl DhcpClient {
             socket,
             local_mac,
             pending_requests,
+            leases: Arc::new(Mutex::new(HashMap::new())),
         })
     }
 
     // Send a request and get back the response with a given transaction ID, or timeout.
-    pub async fn send(&self, xid: u32, bytes: &[u8]) -> Result<Message> {
+    pub async fn send(&self, msg: Message) -> Result<Message> {
+        let xid = msg.xid();
+        let mut buf = Vec::new();
+        let mut e = Encoder::new(&mut buf);
+        msg.encode(&mut e)?;
         let (sender, receiver) = async_std::channel::bounded::<Message>(1);
         self.pending_requests.lock().await.insert(xid, sender);
         self.socket
-            .send_to(bytes, SocketAddr::from(([255, 255, 255, 255], 67)))
+            .send_to(&buf, SocketAddr::from(([255, 255, 255, 255], 67)))
             .await?;
-        let Ok(rcv) = async_std::future::timeout(Duration::from_millis(500), receiver.recv()).await
+        let Ok(rcv) = async_std::future::timeout(
+            Duration::from_millis(DHCP_RESPONSE_TIMEOUT_MS),
+            receiver.recv(),
+        )
+        .await
         else {
             bail!("Timeout")
         };
         rcv.map_err(|_| anyhow!("Channel receive error"))
+    }
+
+    // Obtain and hold a DHCP lease until cancelled.
+    pub async fn obtain_lease(&self, logger: &Logger) -> Result<Ipv4Addr> {
+        println!("Sending DHCP discover");
+        let offer = self.send(discover(&self.local_mac)?).await?;
+        if offer.opts().msg_type() != Some(MessageType::Offer) {
+            bail!("Expected DHCPOFFER in response to DHCPDISCOVER");
+        };
+        println!("Got DHCP offer with IP address {}", offer.yiaddr());
+
+        let request = request_from_offer(&self.local_mac, &offer)?;
+        let ack = self.send(request).await?;
+        if ack.opts().msg_type() != Some(MessageType::Ack) {
+            bail!("Expected DHCPACK in response to DHCPREQUEST");
+        };
+        let Some(v4::DhcpOption::AddressLeaseTime(lease_time)) =
+            ack.opts().get(v4::OptionCode::AddressLeaseTime)
+        else {
+            // RFC2131, table 3
+            bail!("Missing mandatory IP lease time parameter in DHCPACK")
+        };
+        if *lease_time == 0 {
+            bail!("Zero lease time in DHCPACK")
+        };
+
+        // All good.  Spawn the task to keep the lease.
+        let (sender, receiver) = async_std::channel::bounded::<()>(1);
+        self.leases.lock().await.insert(offer.yiaddr(), sender);
+        //let self_clone = Arc::new(self)
+        let logger_clone = logger.clone();
+        let self_clone = self.clone();
+        async_std::task::spawn(
+            async move { keep_lease(receiver, ack, self_clone, logger_clone).await },
+        );
+        Ok(offer.yiaddr())
+    }
+
+    pub async fn cancel_lease(&self, addr: &Ipv4Addr) -> Result<()> {
+        match self.leases.lock().await.remove(addr) {
+            None => bail!("Lease not found"),
+            Some(sender) => sender.send(()).await?,
+        }
+        Ok(())
     }
 }
 
@@ -170,45 +223,45 @@ async fn dispatch_all(
     }
 }
 
-// The DHCP lease object maintains a DHCP lease until cancelled.  Each DHCP lease has a
-// task with a reference to a DhcpClient that periodically makes requests to renew the lease.
-struct DhcpLease {
-    pub ip: Ipv4Addr,
-}
-impl DhcpLease {
-    pub async fn new(client: &DhcpClient) -> Result<Self> {
-        // TODO - try putting the IMSI as the DHCP client ID
-        let (xid, discover) = discover(&client.local_mac)?;
-        println!("Sending DHCP discover");
-        let offer = client.send(xid, &discover).await?;
-        if offer.opts().msg_type() != Some(MessageType::Offer) {
-            bail!("Expected DHCP Offer");
-        };
-        println!("Got DHCP offer with IP address {}", offer.yiaddr());
-        todo!();
+async fn keep_lease(cancel: Receiver<()>, ack: Message, _client: DhcpClient, logger: Logger) {
+    let lease_time_secs = match ack.opts().get(v4::OptionCode::AddressLeaseTime) {
+        Some(v4::DhcpOption::AddressLeaseTime(lease_time)) => *lease_time,
+        _ => 3600, // unhittable - we already checked in the caller
+    };
 
-        // check offer
-        //ensure!(offer == OFFER, "Expected OFFER");
-        // let request = vec![];
-        // let ack = client.send(1, request).await?;
-        // ensure!(ack == ACK, "Expected ACK");
+    // Renew when half the time is up (DHCP timer T1)
+    let renewal_interval = Duration::from_millis(lease_time_secs as u64 * 500);
+    debug!(
+        logger,
+        "DHCP renewal interval {}ms",
+        lease_time_secs as u64 * 500
+    );
 
-        //let addr = ack.address;
-        let ip = Ipv4Addr::new(1, 1, 1, 1);
+    loop {
+        match async_std::future::timeout(renewal_interval, cancel.recv()).await {
+            Err(_) => {
+                // Timeout - renew lease
+                // TODO - note this is unicast and is going to be sent to the UE if we're not careful
+                println!("Lease renewal not yet implemented!!!")
 
-        // All good.  TODO.  Spawn the task to keep the lease.
-        let cancel_sender = 1;
+                // TODO see RFC2131 4.4.5 which has some quite complex behavior around retrying + rebinding
+                // if the ACK does not arrive.
+            }
+            Ok(_) => {
+                // Cancel future completed - end lease
+                // TODO: Probably ok not to implement this for now
+                println!("DHCPRELEASE not yet implemented");
 
-        Ok(Self { ip })
-    }
-    pub async fn cancel(self) {
-        todo!()
+                // Exit this task
+                break;
+            }
+        }
     }
 }
 
 // TODO - have lease drop() spawn a task to carry out the lease cancel procedure?
 
-fn discover(local_mac: &[u8; 6]) -> Result<(u32, Vec<u8>)> {
+fn discover(local_mac: &[u8; 6]) -> Result<Message> {
     let mut msg = v4::Message::default();
     msg.set_flags(v4::Flags::default().set_broadcast()) // set broadcast to true
         .set_chaddr(local_mac) // set chaddr
@@ -225,11 +278,31 @@ fn discover(local_mac: &[u8; 6]) -> Result<(u32, Vec<u8>)> {
             v4::OptionCode::DomainNameServer,
             v4::OptionCode::DomainName,
         ]));
-    let xid = msg.xid();
-    let mut buf = Vec::new();
-    let mut e = Encoder::new(&mut buf);
-    let Ok(()) = msg.encode(&mut e) else {
-        bail!("Failed");
+    Ok(msg)
+}
+
+fn request_from_offer(local_mac: &[u8; 6], offer: &Message) -> Result<Message> {
+    // RFC 2131
+    // The client broadcasts a DHCPREQUEST message
+    // that MUST include the 'server identifier' option to indicate which
+    // server it has selected, and that MAY include other options
+    // specifying desired configuration values.  The 'requested IP
+    // address' option MUST be set to the value of 'yiaddr' in the
+    // DHCPOFFER message from the server.
+    let Some(server_identifier) = offer.opts().get(v4::OptionCode::ServerIdentifier) else {
+        // See RFC2131, table 3
+        bail!("Mandatory option ServerIdentifier missing from DHCP Offer")
     };
-    Ok((xid, buf))
+
+    let mut msg = v4::Message::default();
+    msg.set_flags(v4::Flags::default().set_broadcast())
+        .set_chaddr(local_mac)
+        .opts_mut()
+        .insert(v4::DhcpOption::MessageType(v4::MessageType::Request));
+
+    msg.opts_mut().insert(server_identifier.clone());
+    msg.opts_mut()
+        .insert(v4::DhcpOption::RequestedIpAddress(offer.yiaddr()));
+
+    Ok(msg)
 }
